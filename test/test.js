5f3f9f5cf84ba6840dd60e4501f69a3a9265a562
Meena.Hoda <meena.hoda@wmfs.net>
2018-05-31 13:05:54 +0100

    beginning implementation for testing

--- test/test.js
+++ test/test.js
@@ -1,12 +1,32 @@
 /* eslint-env mocha */
 
 const expect = require('chai').expect
-const cli = require('../lib')
+const exec = require('child_process').exec
 
 describe('tymly-cli tests', function () {
   this.timeout(5000)
 
-  it('should run the function', () => {
-    cli()
+  process.env.TYMLY_API_URL = 'http://localhost:3210'
+
+  it('should run the script', done => {
+    if (
+      process.env.TYMLY_NIC_AUTH0_DOMAIN &&
+      process.env.TYMLY_NIC_AUTH0_CLIENT_ID &&
+      process.env.TYMLY_NIC_AUTH0_CLIENT_SECRET &&
+      process.env.TYMLY_API_URL
+    ) {
+      exec(
+        'npm run cli --name=search --query=kebab --limit=12',
+        (err, stdout, stderr) => {
+          expect(err).to.eql(null)
+          console.log('stdout', stdout)
+          console.log('stderr', stderr)
+          done()
+        }
+      )
+    } else {
+      console.log('Skipping test because missing env vars')
+      done()
+    }
   })
 })
