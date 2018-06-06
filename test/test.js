98fa40a29a8f33dbfb9bf699785b3bb354771e48
Meena.Hoda <meena.hoda@wmfs.net>
2018-06-06 12:53:42 +0100

    fix: Fix for Issue #2: Throw a message if no name parameter provided?

--- test/test.js
+++ test/test.js
@@ -6,27 +6,42 @@ const exec = require('child_process').exec
 describe('tymly-cli tests', function () {
   this.timeout(5000)
 
-  process.env.TYMLY_API_URL = 'http://localhost:3210'
-  it('should run the script', done => {
-    if (
-      process.env.TYMLY_NIC_AUTH0_DOMAIN &&
-      process.env.TYMLY_NIC_AUTH0_CLIENT_ID &&
-      process.env.TYMLY_NIC_AUTH0_CLIENT_SECRET &&
-      process.env.TYMLY_AUTH_AUDIENCE &&
-      process.env.TYMLY_API_URL
-    ) {
+  if (
+    process.env.TYMLY_NIC_AUTH0_DOMAIN &&
+    process.env.TYMLY_NIC_AUTH0_CLIENT_ID &&
+    process.env.TYMLY_NIC_AUTH0_CLIENT_SECRET &&
+    process.env.TYMLY_AUTH_AUDIENCE &&
+    process.env.TYMLY_API_URL
+  ) {
+    it('should run the script', done => {
       exec(
         'npm run cli --name=search --query=kebab --limit=12 --offset=0',
         (err, stdout, stderr) => {
           expect(err).to.eql(null)
-          console.log('stdout', stdout)
-          console.log('stderr', stderr)
           done()
         }
       )
-    } else {
-      console.log('Skipping test because missing env vars')
-      done()
-    }
-  })
+    })
+
+    it('should run the script without a name specified', done => {
+      exec(
+        'npm run cli',
+        (err, stdout, stderr) => {
+          expect(err).to.eql(null)
+          expect(stderr).to.not.eql(null)
+          done()
+        }
+      )
+    })
+  } else {
+    it('should run the script', done => {
+      exec(
+        'npm run cli --name=search --query=kebab --limit=12 --offset=0',
+        (err, stdout, stderr) => {
+          expect(err).to.eql(null)
+          done()
+        }
+      )
+    })
+  }
 })
