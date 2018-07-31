66a035c38968a0d3fe565da08ba97e2ba41f467d
Meena.Hoda <meena.hoda@wmfs.net>
2018-07-31 09:38:12 +0100

    fix: export refreshAll config

--- run-configurations/index.js
+++ run-configurations/index.js
@@ -1,5 +1,6 @@
 module.exports = {
   incidentsInProgress: require('./incidents-in-progress.json'),
+  refreshAll: require('./refresh-all.json'),
   refreshRanking: require('./refresh-ranking.json'),
   search: require('./search.json')
 }
