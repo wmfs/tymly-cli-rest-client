22f98901d152d4ab964cef4901d077bfabb98e50
reecebrend <reece.brend@wmfs.net>
2019-10-18 10:32:52 +0100

    feat: add run-config for saw notifications

--- run-configurations/index.js
+++ run-configurations/index.js
@@ -2,5 +2,6 @@ module.exports = {
   incidentsInProgress: require('./incidents-in-progress.json'),
   refreshAll: require('./refresh-all.json'),
   refreshRanking: require('./refresh-ranking.json'),
-  search: require('./search.json')
+  search: require('./search.json'),
+  checkSawApptNotifications: require('./check-saw-appt-notifications.json')
 }
