93868a747439d378fbb2c17bf9a42b0a3ffe174c
Meena.Hoda <meena.hoda@wmfs.net>
2020-01-27 10:42:13 +0000

    feat: Amend run config to include full reindex state machine and update state machine name for refresh all rankings

--- run-configurations/index.js
+++ run-configurations/index.js
@@ -3,5 +3,6 @@ module.exports = {
   refreshAll: require('./refresh-all.json'),
   refreshRanking: require('./refresh-ranking.json'),
   search: require('./search.json'),
+  fullReindex: require('./full-reindex.json'),
   checkSawApptNotifications: require('./check-saw-appt-notifications.json')
 }
