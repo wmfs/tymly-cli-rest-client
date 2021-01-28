0681b186b0a2069392baf5f071276309f75b2576
Meena.Hoda <meena.hoda@wmfs.net>
2021-01-28 13:12:53 +0000

    feat: add workforce send reminder state machine

--- run-configurations/index.js
+++ run-configurations/index.js
@@ -7,5 +7,6 @@ module.exports = {
   checkSawApptNotifications: require('./check-saw-appt-notifications.json'),
   ppeStockSendReminder: require('./ppe-stock-send-reminder.json'),
   additionalActivitiesSendReminder: require('./additional-activities-send-reminder.json'),
+  workforceSendReminder: require('./workforce-send-reminder.json'),
   sendCnoReminders: require('./send-cno-reminders.json')
 }
